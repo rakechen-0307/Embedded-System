38dd7076e3cba5c069170d6f04cc7a324e19862e
EricLin0123 <150107jasmine@gmail.com>
2024-04-23 15:09:03 +0800

    add hw6

---
+++ hw6/build/CMakeFiles/audio2.dir/DependInfo.cmake
@@ -0,0 +1,81 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/home/wanchuan/embeddedSystem/hw6/audio2/startup_stm32l475xx.s" "/home/wanchuan/embeddedSystem/hw6/audio2/build/CMakeFiles/audio2.dir/startup_stm32l475xx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32L475xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "/home/wanchuan/embeddedSystem/hw6/audio2/cmake/stm32cubemx/../../Core/Inc"
+  "/home/wanchuan/embeddedSystem/hw6/audio2/cmake/stm32cubemx/../../Drivers/STM32L4xx_HAL_Driver/Inc"
+  "/home/wanchuan/embeddedSystem/hw6/audio2/cmake/stm32cubemx/../../Drivers/STM32L4xx_HAL_Driver/Inc/Legacy"
+  "/home/wanchuan/embeddedSystem/hw6/audio2/cmake/stm32cubemx/../../Drivers/CMSIS/Device/ST/STM32L4xx/Include"
+  "/home/wanchuan/embeddedSystem/hw6/audio2/cmake/stm32cubemx/../../Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/wanchuan/embeddedSystem/hw6/audio2/Core/Src/dfsdm.c" "CMakeFiles/audio2.dir/Core/Src/dfsdm.c.obj" "gcc" "CMakeFiles/audio2.dir/Core/Src/dfsdm.c.obj.d"
+  "/home/wanchuan/embeddedSystem/hw6/audio2/Core/Src/dma.c" "CMakeFiles/audio2.dir/Core/Src/dma.c.obj" "gcc" "CMakeFiles/audio2.dir/Core/Src/dma.c.obj.d"
+  "/home/wanchuan/embeddedSystem/hw6/audio2/Core/Src/gpio.c" "CMakeFiles/audio2.dir/Core/Src/gpio.c.obj" "gcc" "CMakeFiles/audio2.dir/Core/Src/gpio.c.obj.d"
+  "/home/wanchuan/embeddedSystem/hw6/audio2/Core/Src/i2c.c" "CMakeFiles/audio2.dir/Core/Src/i2c.c.obj" "gcc" "CMakeFiles/audio2.dir/Core/Src/i2c.c.obj.d"
+  "/home/wanchuan/embeddedSystem/hw6/audio2/Core/Src/main.c" "CMakeFiles/audio2.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/audio2.dir/Core/Src/main.c.obj.d"
+  "/home/wanchuan/embeddedSystem/hw6/audio2/Core/Src/quadspi.c" "CMakeFiles/audio2.dir/Core/Src/quadspi.c.obj" "gcc" "CMakeFiles/audio2.dir/Core/Src/quadspi.c.obj.d"
+  "/home/wanchuan/embeddedSystem/hw6/audio2/Core/Src/spi.c" "CMakeFiles/audio2.dir/Core/Src/spi.c.obj" "gcc" "CMakeFiles/audio2.dir/Core/Src/spi.c.obj.d"
+  "/home/wanchuan/embeddedSystem/hw6/audio2/Core/Src/stm32l4xx_hal_msp.c" "CMakeFiles/audio2.dir/Core/Src/stm32l4xx_hal_msp.c.obj" "gcc" "CMakeFiles/audio2.dir/Core/Src/stm32l4xx_hal_msp.c.obj.d"
+  "/home/wanchuan/embeddedSystem/hw6/audio2/Core/Src/stm32l4xx_it.c" "CMakeFiles/audio2.dir/Core/Src/stm32l4xx_it.c.obj" "gcc" "CMakeFiles/audio2.dir/Core/Src/stm32l4xx_it.c.obj.d"
+  "/home/wanchuan/embeddedSystem/hw6/audio2/Core/Src/syscalls.c" "CMakeFiles/audio2.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/audio2.dir/Core/Src/syscalls.c.obj.d"
+  "/home/wanchuan/embeddedSystem/hw6/audio2/Core/Src/sysmem.c" "CMakeFiles/audio2.dir/Core/Src/sysmem.c.obj" "gcc" "CMakeFiles/audio2.dir/Core/Src/sysmem.c.obj.d"
+  "/home/wanchuan/embeddedSystem/hw6/audio2/Core/Src/system_stm32l4xx.c" "CMakeFiles/audio2.dir/Core/Src/system_stm32l4xx.c.obj" "gcc" "CMakeFiles/audio2.dir/Core/Src/system_stm32l4xx.c.obj.d"
+  "/home/wanchuan/embeddedSystem/hw6/audio2/Core/Src/tim.c" "CMakeFiles/audio2.dir/Core/Src/tim.c.obj" "gcc" "CMakeFiles/audio2.dir/Core/Src/tim.c.obj.d"
+  "/home/wanchuan/embeddedSystem/hw6/audio2/Core/Src/usart.c" "CMakeFiles/audio2.dir/Core/Src/usart.c.obj" "gcc" "CMakeFiles/audio2.dir/Core/Src/usart.c.obj.d"
+  "/home/wanchuan/embeddedSystem/hw6/audio2/Core/Src/usb_otg.c" "CMakeFiles/audio2.dir/Core/Src/usb_otg.c.obj" "gcc" "CMakeFiles/audio2.dir/Core/Src/usb_otg.c.obj.d"
+  "/home/wanchuan/embeddedSystem/hw6/audio2/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal.c" "CMakeFiles/audio2.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal.c.obj" "gcc" "CMakeFiles/audio2.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal.c.obj.d"
+  "/home/wanchuan/embeddedSystem/hw6/audio2/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_cortex.c" "CMakeFiles/audio2.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_cortex.c.obj" "gcc" "CMakeFiles/audio2.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_cortex.c.obj.d"
+  "/home/wanchuan/embeddedSystem/hw6/audio2/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dfsdm.c" "CMakeFiles/audio2.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dfsdm.c.obj" "gcc" "CMakeFiles/audio2.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dfsdm.c.obj.d"
+  "/home/wanchuan/embeddedSystem/hw6/audio2/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma.c" "CMakeFiles/audio2.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma.c.obj" "gcc" "CMakeFiles/audio2.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma.c.obj.d"
+  "/home/wanchuan/embeddedSystem/hw6/audio2/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma_ex.c" "CMakeFiles/audio2.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma_ex.c.obj" "gcc" "CMakeFiles/audio2.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma_ex.c.obj.d"
+  "/home/wanchuan/embeddedSystem/hw6/audio2/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_exti.c" "CMakeFiles/audio2.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_exti.c.obj" "gcc" "CMakeFiles/audio2.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_exti.c.obj.d"
+  "/home/wanchuan/embeddedSystem/hw6/audio2/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash.c" "CMakeFiles/audio2.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash.c.obj" "gcc" "CMakeFiles/audio2.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash.c.obj.d"
+  "/home/wanchuan/embeddedSystem/hw6/audio2/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ex.c" "CMakeFiles/audio2.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/audio2.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ex.c.obj.d"
+  "/home/wanchuan/embeddedSystem/hw6/audio2/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ramfunc.c" "CMakeFiles/audio2.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ramfunc.c.obj" "gcc" "CMakeFiles/audio2.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ramfunc.c.obj.d"
+  "/home/wanchuan/embeddedSystem/hw6/audio2/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_gpio.c" "CMakeFiles/audio2.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_gpio.c.obj" "gcc" "CMakeFiles/audio2.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_gpio.c.obj.d"
+  "/home/wanchuan/embeddedSystem/hw6/audio2/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c.c" "CMakeFiles/audio2.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c.c.obj" "gcc" "CMakeFiles/audio2.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c.c.obj.d"
+  "/home/wanchuan/embeddedSystem/hw6/audio2/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c_ex.c" "CMakeFiles/audio2.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c_ex.c.obj" "gcc" "CMakeFiles/audio2.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c_ex.c.obj.d"
+  "/home/wanchuan/embeddedSystem/hw6/audio2/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pcd.c" "CMakeFiles/audio2.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pcd.c.obj" "gcc" "CMakeFiles/audio2.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pcd.c.obj.d"
+  "/home/wanchuan/embeddedSystem/hw6/audio2/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pcd_ex.c" "CMakeFiles/audio2.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pcd_ex.c.obj" "gcc" "CMakeFiles/audio2.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pcd_ex.c.obj.d"
+  "/home/wanchuan/embeddedSystem/hw6/audio2/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr.c" "CMakeFiles/audio2.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr.c.obj" "gcc" "CMakeFiles/audio2.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr.c.obj.d"
+  "/home/wanchuan/embeddedSystem/hw6/audio2/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr_ex.c" "CMakeFiles/audio2.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr_ex.c.obj" "gcc" "CMakeFiles/audio2.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr_ex.c.obj.d"
+  "/home/wanchuan/embeddedSystem/hw6/audio2/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_qspi.c" "CMakeFiles/audio2.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_qspi.c.obj" "gcc" "CMakeFiles/audio2.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_qspi.c.obj.d"
+  "/home/wanchuan/embeddedSystem/hw6/audio2/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc.c" "CMakeFiles/audio2.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc.c.obj" "gcc" "CMakeFiles/audio2.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc.c.obj.d"
+  "/home/wanchuan/embeddedSystem/hw6/audio2/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc_ex.c" "CMakeFiles/audio2.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/audio2.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc_ex.c.obj.d"
+  "/home/wanchuan/embeddedSystem/hw6/audio2/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_spi.c" "CMakeFiles/audio2.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_spi.c.obj" "gcc" "CMakeFiles/audio2.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_spi.c.obj.d"
+  "/home/wanchuan/embeddedSystem/hw6/audio2/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_spi_ex.c" "CMakeFiles/audio2.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_spi_ex.c.obj" "gcc" "CMakeFiles/audio2.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_spi_ex.c.obj.d"
+  "/home/wanchuan/embeddedSystem/hw6/audio2/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim.c" "CMakeFiles/audio2.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim.c.obj" "gcc" "CMakeFiles/audio2.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim.c.obj.d"
+  "/home/wanchuan/embeddedSystem/hw6/audio2/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim_ex.c" "CMakeFiles/audio2.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/audio2.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim_ex.c.obj.d"
+  "/home/wanchuan/embeddedSystem/hw6/audio2/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart.c" "CMakeFiles/audio2.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart.c.obj" "gcc" "CMakeFiles/audio2.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart.c.obj.d"
+  "/home/wanchuan/embeddedSystem/hw6/audio2/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart_ex.c" "CMakeFiles/audio2.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart_ex.c.obj" "gcc" "CMakeFiles/audio2.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart_ex.c.obj.d"
+  "/home/wanchuan/embeddedSystem/hw6/audio2/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_ll_usb.c" "CMakeFiles/audio2.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_ll_usb.c.obj" "gcc" "CMakeFiles/audio2.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_ll_usb.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ hw6/build/CMakeFiles/audio2.dir/startup_stm32l475xx.s.d
@@ -0,0 +1,2 @@
+CMakeFiles/audio2.dir/startup_stm32l475xx.s.obj: \
+ /home/wanchuan/embeddedSystem/hw6/audio2/startup_stm32l475xx.s
